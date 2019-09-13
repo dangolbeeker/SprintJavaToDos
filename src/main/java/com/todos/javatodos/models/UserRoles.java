5a2f0d8e23ecee2720d456e5f30bedf34e37e05a
dangolbeeker <scottb305@outlook.com>
2019-09-13 13:41:13 -0400

    Todo, Userroles model's

--- src/main/java/com/todos/javatodos/models/UserRoles.java
+++ src/main/java/com/todos/javatodos/models/UserRoles.java
@@ -1,4 +1,60 @@
 package com.todos.javatodos.models;
 
-public class UserRoles extends Auditable {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name ="userroles")
+public class UserRoles extends Auditable implements Serializable
+{
+
+//    USERROLES
+//
+//    roleid foreign key to role
+//    userid foreign key to user
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    @JoinColumn(name = "userid")
+    @JsonIgnoreProperties("userRoles")
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @ManyToOne
+    @JoinColumn(name ="roleid")
+    @JsonIgnoreProperties("userRoles")
+    private Role role;
+
+    public UserRoles()
+    {
+
+    }
+
+
+    public UserRoles(User user, Role role)
+    {
+        this.user = user;
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
